25a2ea0662a3982e973d24b7bc009ee0e7d4df4e
Philipp <pd95@users.noreply.github.com>
2022-04-30 18:55:27 +0200

    Initial Commit

---
+++ SimpleScores/SimpleScoresApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleScoresApp.swift
+//  SimpleScores
+//
+//  Created by Philipp on 30.04.22.
+//
+
+import SwiftUI
+
+@main
+struct SimpleScoresApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
